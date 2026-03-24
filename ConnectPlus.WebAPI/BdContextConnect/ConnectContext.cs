33e47e8ee5ecf94d25ecdb9c2ff1946d42268892
HENRIQUE DO NASCIMENTO <42916360832@info.local>
2026-03-24 16:48:59 -0300

    atividade connect plus

---
+++ ConnectPlus.WebAPI/BdContextConnect/ConnectContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using ConnectPlus.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectPlus.WebAPI.BdContextConnect;
+
+public partial class ConnectContext : DbContext
+{
+    public ConnectContext()
+    {
+    }
+
+    public ConnectContext(DbContextOptions<ConnectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contato> Contatos { get; set; }
+
+    public virtual DbSet<TipoDeContato> TipoDeContatos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ConnectPlus;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contato>(entity =>
+        {
+            entity.HasKey(e => e.IdContato).HasName("PK__Contato__2AC4F06451F7B3F3");
+
+            entity.Property(e => e.IdContato).HasDefaultValueSql("(newid())");
+
+            entity.HasOne(d => d.IdTipoContatoNavigation).WithMany(p => p.Contatos).HasConstraintName("FK__Contato__IdTipoC__75A278F5");
+        });
+
+        modelBuilder.Entity<TipoDeContato>(entity =>
+        {
+            entity.HasKey(e => e.TipoDeContatoId).HasName("PK__TipoDeCo__3528C6FF0826D2C7");
+
+            entity.Property(e => e.TipoDeContatoId).HasDefaultValueSql("(newid())");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ConnectPlus.WebAPI/Models/Contato.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectPlus.WebAPI.Models;
+
+[Table("Contato")]
+[Index("TipoDeContato", Name = "UQ__Contato__1BB80D302DB0B584", IsUnique = true)]
+[Index("FormaDeContato", Name = "UQ__Contato__DCCB6F48C8D4EBC2", IsUnique = true)]
+public partial class Contato
+{
+    [Key]
+    public Guid IdContato { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    [Column("formaDeContato")]
+    [StringLength(256)]
+    [Unicode(false)]
+    public string FormaDeContato { get; set; } = null!;
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Imagem { get; set; } = null!;
+
+    [Column("tipoDeContato")]
+    [StringLength(256)]
+    [Unicode(false)]
+    public string TipoDeContato { get; set; } = null!;
+
+    public Guid? IdTipoContato { get; set; }
+
+    [ForeignKey("IdTipoContato")]
+    [InverseProperty("Contatos")]
+    public virtual TipoDeContato? IdTipoContatoNavigation { get; set; }
+}
